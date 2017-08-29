3ec301534dadf3ba8d8c5346a4b9b85c027e8757
sarah mbaka <salliedailey@gmail.com>
2017-08-29 15:41:07 +0300

    Alert users when Bucketlist successfully deleted

--- src/components/deleteBucket/index.js
+++ src/components/deleteBucket/index.js
@@ -5,19 +5,13 @@ import TextField from 'material-ui/TextField';
 import { hashHistory } from "react-router";
 
 
-/**
- * Dialog with action buttons. The actions are passed in as an array of React objects,
- * in this example [FlatButtons](/#/components/flat-button).
- *
- * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
- */
 class DeleteBucket extends Component {
   constructor(props){
     super(props);
     // default state
     this.state = {
       "open": false,
-      "erroropen": false,
+      "_open": false,
       "token": "",
       "dialog_msg": "Are you sure you want to delete this Item?",
       "items": []
@@ -35,27 +29,24 @@ class DeleteBucket extends Component {
 
   handleClose = () => {
     this.setState({open: false});
+    this.setState({_open: false});
   };
 
   handleSubmit(event, bucket_id){
-    // prevent the default browser action
     event.preventDefault();
 
     if (this.state.name === "" || this.state.description === "") {
-        // error empty inputs
     }
     this.deletebucketlist(bucket_id);
     this.setState({open: false});
   }
   componentDidMount(){
     if(typeof(localStorage) !==  undefined){
-      // store the token
         const token = localStorage.getItem("buckapi_token");
         if (token === null) {
           alert("token not found, please login again");
         }
         else {
-          //console.log("token", token);
           this.setState({
               token: token,
           });
@@ -78,21 +69,17 @@ class DeleteBucket extends Component {
        })
       .then((resp) => resp.json()) // Transform the data into json
       .then(function(data) {
-        // Create and append the li's to the ul
-        console.log(data);
           if(data.status === "success"){
-            // login was successful
+            console.log(data);
             _this.setState({
               dialog_msg: data.message,
-              open: true
+              _open: true
             })
-
-              hashHistory.push("/dashboard");
           }
           else{
             _this.setState({
-              error: data.message,
-              // open: true
+              dialog_msg: data.message,
+              _open: true
             })
           }
       }).catch((err) =>{
@@ -115,12 +102,30 @@ class DeleteBucket extends Component {
 
     ];
 
+    const action = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+    ];
+
     return (
       <div>
         <span className="symbol"><img src={process.env.PUBLIC_URL + "/images/delete.svg"} onClick={(event) => this.handleOpen(event)}  /></span>
 
         <Dialog
-          title="Edit Bucketlist"
+          title="Success"
+          actions={action}
+          modal={false}
+          open={this.state._open}
+          onRequestClose={this.handleClose}
+        >
+        <p> {this.state.dialog_msg}  </p>
+        </Dialog>
+
+        <Dialog
+          title="Delete Bucketlist"
           actions={actions}
           modal={false}
           open={this.state.open}
